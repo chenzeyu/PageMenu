302dc423d9f1ae5d7d5cdfca4a93befe50f49ea8
Chen Zeyu <zeyufly@gmail.com>
2017-04-21 21:28:55 +0800

    change width calculation

--- Classes/MenuItemView.swift
+++ Classes/MenuItemView.swift
@@ -54,7 +54,7 @@ class MenuItemView: UIView {
         }
         
         // Configure menu item label font if font is set by user
-        self.titleLabel!.font = pageMenu.configuration.unselectedMenuItemFont
+        self.titleLabel!.font = pageMenu.configuration.selectedMenuItemFont
         
         self.titleLabel!.textAlignment = NSTextAlignment.center
         self.titleLabel!.textColor = pageMenu.configuration.unselectedMenuItemLabelColor
