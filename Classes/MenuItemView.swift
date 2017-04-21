3a6c93e4745440923e451b4c7add9a302eae6f79
Chen Zeyu <zeyufly@gmail.com>
2017-04-21 21:24:24 +0800

    add selectedMenuFont and unselectedMenuFont

--- Classes/MenuItemView.swift
+++ Classes/MenuItemView.swift
@@ -54,7 +54,7 @@ class MenuItemView: UIView {
         }
         
         // Configure menu item label font if font is set by user
-        self.titleLabel!.font = pageMenu.configuration.menuItemFont
+        self.titleLabel!.font = pageMenu.configuration.unselectedMenuItemFont
         
         self.titleLabel!.textAlignment = NSTextAlignment.center
         self.titleLabel!.textColor = pageMenu.configuration.unselectedMenuItemLabelColor
